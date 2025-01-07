6631988be14a46a6ce4ec74b9b2f1c95dd14550f
Brian Sabatini <brian_sabatini1993@hotmail.com>
2025-01-07 03:00:07 -0300

    colores de btn neon definido y listo, falta forms

--- src/components/ImputRoms/ImputRoms.jsx
+++ src/components/ImputRoms/ImputRoms.jsx
@@ -7,25 +7,28 @@ export const ImputRoms = () => {
     const root = document.querySelector(":root");
     if (botonNeon) {
       root.style.setProperty("--color-fuente", "rgba(240, 240, 250, 0.8)");
-      root.style.setProperty("--color-navbar", "none");
-      root.style.setProperty("--color-footer", "rgb(10,10,10)");
-      root.style.setProperty("--color-border", "rgba(100, 0, 0, 0.8)");
-      root.style.setProperty("--color-border-tv-parlantes", "rgba(200, 0, 0, 0.7)");
-      root.style.setProperty("--color-background-section", "rgba(0, 0, 0, 0.95)");
-      root.style.setProperty("--color-card-gradiante-mid", "rgba(188, 0, 0, 0.55)");
-      root.style.setProperty("--color-card-background", "rgba(155, 0, 0, 0.1)");
-      root.style.setProperty("--color-shadow", "rgba(100, 0, 0, 0.5)");
+      root.style.setProperty("--color-navbar", "rgba(151, 151, 151, 0.1)");
+      root.style.setProperty("--color-footer", "rgb(6,6,6)");
+      root.style.setProperty("--color-border", "rgba(151, 151, 151, 0.8)");
+      root.style.setProperty("--color-border-tv-parlantes", "rgba(255, 190, 0, 0.9)");
+      root.style.setProperty("--color-background-section", "rgba(0, 0, 0, 0.9)");
+      root.style.setProperty("--color-card-gradiante-mid", "rgba(211, 211, 211, 0.05)");
+      root.style.setProperty("--color-card-background", "rgba(0, 0, 0, 0.1)");
+      root.style.setProperty("--color-shadow", "rgba(0, 0, 0, 0.6)");
+      root.style.setProperty("--color-bar", " rgba(111, 111, 111, 0.9)");
     } else {
       root.style.setProperty("--color-fuente", "rgba(236, 171, 59, 0.8)");
+      root.style.setProperty("--color-navbar"," rgba(200, 0, 250, 0.95)");
       root.style.setProperty("--color-footer", "rgb(245,245,245)");
       root.style.setProperty("--color-border", "rgba(200, 0, 250, 0.8)");
       root.style.setProperty("--color-border-tv-parlantes", "rgba(200, 0, 200, 0.9)");
       root.style.setProperty("--color-background-section", "rgba(50, 0, 100, 0.72)");
-      root.style.setProperty("--color-card-gradiante-mid", "rgba(200, 0, 255, 0.3)");
+      root.style.setProperty("--color-card-gradiante-mid", "rgba(200, 0, 255, 0.4)");
       root.style.setProperty("--color-card-background", "rgba(200, 0, 250, 0.1)");
       root.style.setProperty("--color-shadow", "rgba(200, 0, 250, 0.6)");
-      
+      root.style.setProperty("--color-bar", " rgba(200, 0, 250, 0.95)");
     }
+    
     setBotonNeon(!botonNeon);
   };
 
