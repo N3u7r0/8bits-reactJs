cff8a6905dccad4a09c0563a64b5e0d574ca6536
Brian Sabatini <brian_sabatini1993@hotmail.com>
2025-01-17 00:22:23 -0300

    refactory de BtnDark

--- src/components/BtnDark/BtnDark.jsx
+++ src/components/BtnDark/BtnDark.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const BtnDark = () => {
   const [botonNeon, setBotonNeon] = useState(true);
@@ -10,27 +10,37 @@ export const BtnDark = () => {
       root.style.setProperty("--color-navbar", "rgba(151, 151, 151, 0.1)");
       root.style.setProperty("--color-footer", "rgb(6,6,6)");
       root.style.setProperty("--color-border", "rgba(151, 151, 151, 0.8)");
-      root.style.setProperty("--color-border-tv-parlantes", "rgba(255, 190, 0, 0.9)");
-      root.style.setProperty("--color-background-section", "rgba(0, 0, 0, 0.9)");
-      root.style.setProperty("--color-card-gradiante-mid", "rgba(211, 211, 211, 0npm.05)");
+      root.style.setProperty("--color-border-tv-parlantes","rgba(255, 190, 0, 0.9)");
+      root.style.setProperty("--color-background-section","rgba(0, 0, 0, 0.9)");
+      root.style.setProperty("--color-card-gradiante-mid","rgba(211, 211, 211, 0npm.05)");
       root.style.setProperty("--color-card-background", "rgba(0, 0, 0, 0.1)");
       root.style.setProperty("--color-shadow", "rgba(0, 0, 0, 0.6)");
       root.style.setProperty("--color-bar", " rgba(111, 111, 111, 0.9)");
     } else {
-      root.style.setProperty("--color-fuente", "rgba(236, 171, 20, 0.7)");
-      root.style.setProperty("--color-navbar"," rgba(200, 0, 250, 0.95)");
-      root.style.setProperty("--color-footer", "rgb(245,245,245)");
-      root.style.setProperty("--color-border", "rgba(200, 0, 250, 0.8)");
-      root.style.setProperty("--color-border-tv-parlantes", "rgba(200, 0, 200, 0.9)");
-      root.style.setProperty("--color-background-section", "rgba(50, 0, 100, 0.72)");
-      root.style.setProperty("--color-card-gradiante-mid", "rgba(200, 0, 255, 0.4)");
-      root.style.setProperty("--color-card-background", "rgba(200, 0, 250, 0.1)");
-      root.style.setProperty("--color-shadow", "rgba(200, 0, 250, 0.6)");
-      root.style.setProperty("--color-bar", " rgba(200, 0, 250, 0.95)");
+      root.style.removeProperty("--color-fuente");
+      root.style.removeProperty("--color-navbar");
+      root.style.removeProperty("--color-footer");
+      root.style.removeProperty("--color-border");
+      root.style.removeProperty("--color-border-tv-parlantes");
+      root.style.removeProperty("--color-background-section");
+      root.style.removeProperty("--color-card-gradiante-mid");
+      root.style.removeProperty("--color-card-background");
+      root.style.removeProperty("--color-shadow");
+      root.style.removeProperty("--color-bar");
+
     }
-    
+    localStorage.setItem("btnDark", JSON.stringify(botonNeon));
     setBotonNeon(!botonNeon);
   };
+
+  let storage = JSON.parse(localStorage.getItem("btnDark"));
+
+  useEffect(() => {
+  if(storage){
+    logicaBtnNeon();
+  }
+  }, []);
+
 
   return (
     <button onClick={logicaBtnNeon} type="button" className="btn_1up">
